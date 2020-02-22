35984a7ea1bee39cf7a99c921167fa7ad8a21f43
Matheus Rodrigues da Cruz <matheus.rcruz12@gmail.com>
2020-02-22 18:07:37 -0300

    add redux

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,95 +1,57 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { MdShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
+
 import { ProdictList } from './styles';
 
-export default function Home() {
-  return (
-    <ProdictList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/bota-caterpillar-comfort-fielder-masculino/10/NGF-0065-010/NGF-0065-010_zoom2.jpg?ims=326x"
-          alt="tenis"
-        />
-        <strong>Bota Masc</strong>
-        <span>R$129,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Adicionar no carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/bota-caterpillar-comfort-fielder-masculino/10/NGF-0065-010/NGF-0065-010_zoom2.jpg?ims=326x"
-          alt="tenis"
-        />
-        <strong>Bota Masc</strong>
-        <span>R$129,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Adicionar no carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/bota-caterpillar-comfort-fielder-masculino/10/NGF-0065-010/NGF-0065-010_zoom2.jpg?ims=326x"
-          alt="tenis"
-        />
-        <strong>Bota Masc</strong>
-        <span>R$129,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Adicionar no carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/bota-caterpillar-comfort-fielder-masculino/10/NGF-0065-010/NGF-0065-010_zoom2.jpg?ims=326x"
-          alt="tenis"
-        />
-        <strong>Bota Masc</strong>
-        <span>R$129,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Adicionar no carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/bota-caterpillar-comfort-fielder-masculino/10/NGF-0065-010/NGF-0065-010_zoom2.jpg?ims=326x"
-          alt="tenis"
-        />
-        <strong>Bota Masc</strong>
-        <span>R$129,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Adicionar no carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/bota-caterpillar-comfort-fielder-masculino/10/NGF-0065-010/NGF-0065-010_zoom2.jpg?ims=326x"
-          alt="tenis"
-        />
-        <strong>Bota Masc</strong>
-        <span>R$129,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Adicionar no carrinho</span>
-        </button>
-      </li>
-    </ProdictList>
-  );
+class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('/products');
+
+    const listProducts = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: listProducts });
+  }
+  handleAddProduct = product => {
+    const { dispatch } = this.props;
+
+    dispatch({ type: 'ADD_TO_CART', product });
+  };
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProdictList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.price}</span>
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product)}
+            >
+              <div>
+                <MdShoppingCart size={16} color="#fff" /> 3
+              </div>
+              <span>Adicionar no carrinho</span>
+            </button>
+          </li>
+        ))}
+      </ProdictList>
+    );
+  }
 }
+
+export default connect()(Home);
+// Home;
---
+++ src/util/format.js
@@ -0,0 +1,4 @@
+export const { format: formatPrice } = new Intl.NumberFormat('pt-BR', {
+  styde: 'currency',
+  currency: 'BRL',
+});
